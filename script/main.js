f5234feab8cf32e7bc2c91d6ff29c98b1cf59a0f
Sanne <sannevanzeijl@hotmail.com>
2019-02-28 12:53:46 +0100

    datum maand jaar werkt niet

--- script/main.js
+++ script/main.js
@@ -3,15 +3,20 @@ function startTime() {
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
+  var d = today.getDate();
+  var mnd =  today.getMonth();
+  var y = today.getFullYear();
   m = checkTime(m);
   s = checkTime(s);
   document.getElementById('time').innerHTML =
   h + ":" + m + ":" + s;
+  documet.getElementById('demo').innerHTML = 
+  d + mnd + y
   var t = setTimeout(startTime, 500);
 
-  var d = new Date();
-  var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-  document.getElementById("demo").innerHTML = months[d.getMonth()];
+  // var d = new Date();
+  // var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  // document.getElementById("demo").innerHTML = months[d.getMonth()];
 
   var d = new Date();
   document.getElementById("day").innerHTML = d.getDate();
